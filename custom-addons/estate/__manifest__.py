8ef59faffdd3adb9859352af0d7bff364c7444d3
giangdhptit <giang.dinh@lifesup.local>
2024-07-04 12:01:14 +0700

    first commit

---
+++ custom-addons/estate/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"Estate",
+    "version":"1.0",
+    "website":"https//www.theodooguys.com",
+    "author":"Giang Bi",
+    "description":"""
+        Description
+    """,
+    "depends":[],
+    "category":"Sales",
+    "data":[],
+    "installable": True,
+    "application": True,
+    "license":"LGPL-3",
+}
